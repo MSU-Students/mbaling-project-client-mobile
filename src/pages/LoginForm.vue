c90d63842493a0d30ee7aed98379cc268be92175
Abdul Moiz Solaiman <solaiman.am85@s.msumain.edu.ph>
2021-09-15 17:30:19 +0800

    Changes in page design/css

--- src/pages/LoginForm.vue
+++ src/pages/LoginForm.vue
@@ -1,68 +1,55 @@
 <template>
-  <q-page
-    class="window-heightwindow-width row justify-center items-center"
-    style="background-color: #be272c"
-  >
-    <div class="absolute-center">
-      <q-card-section>
-        <q-form class="">
-          <!-- LogoPicture -->
-          <div class="row justify-center q-pa-xl q-gutter-xl">
-            <q-avatar square size="6rem">
-              <img src="../assets/mbaling-logo-vertical.svg" />
-            </q-avatar>
-          </div>
-
-          <!-- UserName&PasswordInput -->
-          <div class="q-gutter-sd" style="max-width: 300px">
-            <q-input
-              dark
-              dense
-              input-class="text-center"
-              color="white"
-              v-model="username"
-              type="username"
-              placeholder="username"
-            >
-            </q-input>
-
-            <q-input
-              dark
-              dense
-              color="white"
-              input-class="text-center"
-              v-model="password"
-              type="password"
-              placeholder="password"
-            >
-            </q-input>
-          </div>
-
-          <!-- LoginButton -->
-          <div class="row justify-center q-pa-md q-gutter-sm">
-            <q-btn
-              :ripple="false"
-              unelevated
-              dense
-              rounded
-              no-caps
-              class="text-red text-bold"
-              style="height: 1.5rem; width: 6rem; font-size: small"
-              color="white"
-              label="log-in"
-              @click="loginUser()"
-            />
-          </div>
-        </q-form>
-      </q-card-section>
-    </div>
+  <q-page class="row items-center justify-evenly">
+    <q-card flat class="bg-transparent text-center">
+      <!-- LOGO PICTURE -->
+      <div>
+        <q-img
+          src="~assets/mbaling-logo-vertical.svg"
+          style="max-width: 6rem"
+        />
+      </div>
+      <!-- USERNAME & PASSWORD INPUT -->
+      <div class="q-mt-xl">
+        <q-input
+          dark
+          dense
+          input-class="text-center"
+          color="white"
+          v-model="username"
+          type="username"
+          placeholder="username"
+        />
+        <q-input
+          dark
+          dense
+          color="white"
+          input-class="text-center"
+          v-model="password"
+          type="password"
+          placeholder="password"
+        />
+        <!-- LOG-IN BUTTON -->
+        <q-btn
+          :ripple="false"
+          unelevated
+          rounded
+          dense
+          no-caps
+          class="text-red text-bold q-mt-md"
+          style="height: 1.5rem; width: 6rem; font-size: smaller"
+          color="white"
+          label="log-in"
+          @click="loginUser()"
+        />
+      </div>
+    </q-card>
   </q-page>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 export default {
-  name: "Login",
+  name: "LoginForm",
   setup() {
     return {
       username: ref(""),
--- src/pages/student/StudentAccount.vue
+++ src/pages/student/StudentAccount.vue
@@ -1,65 +1,86 @@
 <template>
-  <q-header style="height:3rem;background-color:transparent">
+  <q-header style="height: 3rem; background-color: transparent">
     <q-toolbar>
-        <q-space/>
-        <q-btn color="black" flat round dense icon="more_vert"> </q-btn>
+      <q-space />
+      <q-btn
+        dense
+        flat
+        :ripple="false"
+        color="black"
+        icon="bi-three-dots-vertical"
+      />
     </q-toolbar>
   </q-header>
 
- <q-toolbar
-    class="text-white profile_bg column items-center justify-center"
-  >
-
-  <!-- PROFILE TOP -->
-    <q-avatar
-      class="q-mb-md q-mt-lg"
-      size="120px"
-      font-size="52px"
-      color="grey"
-      text-color="white"
-      icon="account_circle"
-    />
-    <div class="column items-center justify-center">
-      <p class="info-username">@{{ sProfile.username }}</p>
-
-      <p class="info-fullname defaultfont-bold text-uppercase">{{ sProfile.name }}</p>
-      <p class="info-other">
-        {{ sProfile.id }}<br />{{ sProfile.sDegree }}, {{sProfile.sYear}} <br />
-        {{ sProfile.sDepartment }}<br />
-        {{ sProfile.sCollege }}<br />
-      </p>
+  <q-page class="defaultfont bg-secondary text-black">
+    <!-- PROFILE SECTION -->
+    <div class="bg-primary flex-center text-center q-pa-md">
+      <q-avatar class="q-mt-md" size="8rem">
+        <img :src="student.prfphoto" />
+      </q-avatar>
+      <div class="text-white q-mt-lg">
+        <p class="info-username">{{ student.username }}</p>
+        <span class="defaultfont-bold info-fullname text-uppercase">
+          {{ student.fullname }}
+        </span>
+        <p class="info-other">
+          {{ student.studentID }} <br />
+          {{ student.degree }}, {{ student.yearAdmit }} <br />
+          {{ student.department }} <br />
+          {{ student.college }}
+        </p>
+      </div>
     </div>
-  </q-toolbar>
-   <q-tabs
-        inline-label
+
+    <!-- BOOKMARK & APPLICATION TAB -->
+    <div class="q-mt-sm">
+      <q-tabs
         v-model="tab"
         dense
-        color="secondary"
-        class="text-grey"
+        inline-label
+        class="defaultfont-semibold text-grey q-px-md"
+        style="max-height: 2rem"
         active-color="black"
         indicator-color="transparent"
         align="justify"
-        narrow-indicator
       >
-        <q-tab  :ripple="false" icon="bi-bookmark-fill" name="bookmark" label="BOOKMARK" />
-        <q-tab  :ripple="false" icon='bi-file-text-fill' name="application" label="APPLICATION" />
+        <q-tab :ripple="false" name="bookmarks">
+          <p class="row items-center" style="font-size: smaller">
+            <q-icon name="bi-bookmark-fill" class="q-mr-xs" size="1rem" />
+            BOOKMARK
+          </p>
+        </q-tab>
+        <q-tab :ripple="false" name="applications">
+          <p class="row items-center" style="font-size: smaller">
+            <q-icon
+              name="bi-file-earmark-text-fill"
+              class="q-mr-xs"
+              size="1rem"
+            />
+            APPLICATIONS
+          </p>
+        </q-tab>
       </q-tabs>
-      
-      <hr class="bg-black q-mt-none" style="width:87%"/>
 
-      <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="bookmark">
-          <q-card class="my-card" flat bordered>
+      <q-separator inset size="0.1rem" color="black" />
+
+      <q-tab-panels v-model="tab" animated class="bg-transparent">
+        <!-- BOOKMARK TAB PANEL -->
+        <q-tab-panel name="bookmarks">
+          <q-card flat bordered>
             <q-item>
               <q-item-section avatar>
                 <q-avatar icon="account_circle" />
               </q-item-section>
 
               <q-item-section class="post-user">
-              <div>  <q-item-label>Hello Raian </q-item-label> </div>
-              <div>  <q-item-label caption> Muammar's Boarding House </q-item-label> </div>
-                     <q-item-label caption> 3 days ago </q-item-label>
-              
+                <div><q-item-label>Hello Raian </q-item-label></div>
+                <div>
+                  <q-item-label caption>
+                    Muammar's Boarding House
+                  </q-item-label>
+                </div>
+                <q-item-label caption> 3 days ago </q-item-label>
               </q-item-section>
             </q-item>
 
@@ -71,72 +92,75 @@
               <div class="view-more">•View more</div>
               <hr class="bg-red-14" />
               <q-toolbar>
-                
                 <div class="button-options" style="font-size: 20px">
-                
                   <q-icon name="bi-heart" />
                   <q-icon class="q-ml-xs" name="bi-bookmark" />
                   <q-icon class="q-ml-xs" name="bi-chat-right" />
-                  <div class="view-more">44 likes • 1 bookmarks</div> 
-                  </div>
+                  <div class="view-more">44 likes • 1 bookmarks</div>
+                </div>
               </q-toolbar>
             </q-card-section>
           </q-card>
         </q-tab-panel>
 
-         <q-tab-panel name="application">
-          <q-item>
-            <q-item-section thumbnail class="q-ml-none">
-              <img src="~src\assets\post-picture-a002.jpg" />
-            </q-item-section>
+        <!-- APPLICATION TAB PANEL-->
+        <q-tab-panel name="applications">
+          <q-card flat bordered>
+            <q-item class="bg-white">
+              <q-item-section thumbnail class="q-ml-none">
+                <img src="~src\assets\post-picture-a002.jpg" />
+              </q-item-section>
 
-            <q-item-section>
-             <div> <q-item-label>Hello Raian</q-item-label></div>
-              <q-item-label caption class="q-mt-none q-pt-none">Muammar's Boarding House </q-item-label>
-            <div class="button-apply">
-              <q-btn
-              style="width: 20%"
-              padding="xs"
-              size="0.5rem"
-              color="primary"
-              label="Apply"
-            />
-            </div>
-            </q-item-section>
-              </q-item>
+              <q-item-section>
+                <div><q-item-label>Hello Raian</q-item-label></div>
+                <q-item-label caption class="q-mt-none q-pt-none"
+                  >Muammar's Boarding House
+                </q-item-label>
+                <div class="button-apply">
+                  <q-btn
+                    style="width: 20%"
+                    padding="xs"
+                    size="0.5rem"
+                    color="primary"
+                    label="Apply"
+                  />
+                </div>
+              </q-item-section>
+            </q-item>
+          </q-card>
         </q-tab-panel>
-
       </q-tab-panels>
-  <q-page>
+    </div>
   </q-page>
-
 </template>
 
-<script>
+<script lang="ts">
+import { ref } from "vue";
 export default {
   data() {
     return {
-      sProfile: {
-        name: "Last M. First",
-        id: "201845823",
-        username: "Username1234",
-        sDegree: "BS Information Technology (Database)",
-        sDepartment: "Computer Studies Department",
-        sCollege: "College of Information Technology",
-        sYear: "2018"
+      student: {
+        username: "@palawanexpress98",
+        prfphoto: "https://cdn.quasar.dev/img/boy-avatar.png",
+        studentID: 201812730,
+        fullname: "Nahed S. Bashier",
+        degree: "BS Information Technology (Database System)",
+        yearAdmit: 2018,
+        department: "Department of Information Sciences",
+        college: "College of Information Technology",
       },
-      lProfile:{
-        
-
-      },
-       tab: "mails",
+    };
+  },
+  setup() {
+    return {
+      tab: ref("mails"),
     };
   },
 };
 </script>
 
 <style lang="sass">
-  .card-post
-    .q-img
-      min-width: 100px
+.card-post
+  .q-img
+    min-width: 100px
 </style>
