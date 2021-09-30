bb49884a1efbf6df95af4667b922a1e930c11af6
Abdulwalie <abdulwalie5@gmail.com>
2021-09-30 13:38:12 +0800

    added a temporary chat

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,5 +1,9 @@
 <template>
-    <q-page>
-        <h5>CHAT PAGE</h5>
-    </q-page>
+<q-page class="flex column">
+    <div class="q-pa-md column col justify-end">
+    <q-chat-message name="me" :text="['hey, how are you?']" sent />
+    <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
+    </div>
+ 
+  </q-page>
 </template>
