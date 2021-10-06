a928a586471552b4aa791b1788812def2c330e9a
Abdulwalie <abdulwalie5@gmail.com>
2021-10-06 15:42:43 +0800

    add application on top

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,12 +1,45 @@
 <template>
     <q-page class="flex column">
-    <q-header elevated class="bg-white q-px-md q-py-sm" style="height: 3rem">
-      <q-toolbar-title>
-        
-      </q-toolbar-title>
+    <q-header elevated class="bg-white" style="height: 3rem">
+      <q-toolbar >
+         <q-avatar> <q-img src="https://cdn.quasar.dev/img/avatar2.jpg" class="chat-avatar"/> </q-avatar>
+         <q-toolbar-title class="text-black text-subtitle2">Azshara Highborne</q-toolbar-title>
+
+          <q-btn 
+          class="q-pr-none"
+          color="black"
+          flat
+          icon="arrow_back" 
+          :ripple="false"         
+         @click="() => $router.replace('/inbox')"/>
+      </q-toolbar>
     </q-header>
-<!-- :color="user.online ? 'light-green-5' : 'grey-4'"> -->
-   <div class="q-pa-md column col justify-end">
+    <q-card flat bordered class="q-mt-md">
+            <q-item class="bg-white">
+              <q-item-section thumbnail class="q-ml-none">
+                <img src="https://cdn.quasar.dev/img/parallax1.jpg" class="q-ml-sm" />
+              </q-item-section>
+
+              <q-item-section>
+                <div><q-item-label>Hello Raian</q-item-label></div>
+                <q-item-label caption class="q-mt-none q-pt-none"
+                  >Muammar's Boarding House
+                </q-item-label>
+                <div class="button-apply">
+                  <q-btn
+                    style="width: 20%"
+                    padding="xs"
+                    size="0.5rem"
+                    color="primary"
+                    label="Apply"
+                  />
+                </div>
+              </q-item-section>
+            </q-item>
+          </q-card>
+
+<q-scroll-area class="q-pa-md column col justify-end" style="height: 38rem; max-width: 100%;">
+   <div >
       <q-chat-message
         v-for="message in messages"
         :text-color="message.from == 'me' ? 'white' : 'black'"
@@ -16,6 +49,8 @@
         :sent="message.from == 'me' ? true : false"
       />
     </div>
+    </q-scroll-area>
+  
 
     <q-footer class="bg-white">
       <q-toolbar>
@@ -27,15 +62,21 @@
            rounded
            outlined 
            v-model="newMessage"
-           label="Message" 
+           placeholder="Message"
            dense>
 
             <template v-slot:append>
-              <q-icon name="send" @click="sendMessage" />
+              <q-btn
+              class=" q-pa-none"
+              flat
+              icon="send"
+              :disable="!newMessage" 
+              name="send" 
+              @click="sendMessage" />
             </template>
 
             <template v-slot:after>
-              <q-btn round dense flat icon="add_circle_outline" />
+              <q-btn round dense flat icon="add_circle_outline" @click="show(true)" />
             </template>
           </q-input>
         </q-toolbar-title>
@@ -47,6 +88,7 @@
 </template>
 
 <script>
+import { Ripple, useQuasar } from 'quasar'
 export default {
   data(){
     return{
@@ -75,6 +117,46 @@ export default {
       })
       this.newMessage = ''
     }
+  },
+  setup () {
+    const $q = useQuasar()
+
+    function show (grid) {
+      $q.bottomSheet({
+        Ripple,
+        message: 'More',
+        grid,
+        actions: [
+          {
+            label: 'Gallery',
+            icon: 'bi-image',
+            id: 'drive',
+          },
+          {
+            label: 'Camera',
+            icon: 'bi-camera',
+            id: 'camera'
+          },
+          {
+            label: 'Link',
+            icon: 'bi-door-closed',
+            id: 'link'
+          },
+          {},
+        ]
+      }).onOk(action => {
+        // console.log('Action chosen:', action.id)
+      }).onCancel(() => {
+        // console.log('Dismissed')
+      }).onDismiss(() => {
+        // console.log('I am triggered on both OK and Cancel')
+      })
+    }
+
+    return { show }
   }
 }
 </script>
+<style>
+
+</style>
