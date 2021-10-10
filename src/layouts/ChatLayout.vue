5b086767ec61b1fc96c7c1bef03745fd4f549160
Abdulwalie <abdulwalie5@gmail.com>
2021-10-10 19:43:02 +0800

    make chatLayout usable to postForm

--- src/layouts/ChatLayout.vue
+++ src/layouts/ChatLayout.vue
@@ -1,6 +1,36 @@
 <template>
   <q-layout class="bg-grey-3" view="hHh lpR fFf">
 
+     <q-header elevated class="bg-white" style="height: 3rem">
+      <q-toolbar >
+         <q-avatar v-if="$route.fullPath.includes('/chat')"> 
+            <q-img src="https://cdn.quasar.dev/img/avatar2.jpg" class="chat-avatar"/> 
+            </q-avatar>
+         <q-toolbar-title 
+            v-if="$route.fullPath.includes('/chat')"
+            class="text-black text-subtitle2">Azshara Highborne</q-toolbar-title>
+
+          <q-btn 
+          class="q-px-none"
+          color="black"
+          flat
+          icon="arrow_back" 
+          :ripple="false"         
+           @click="$router.go(-1)"/>
+
+        <q-space v-if="$route.fullPath.includes('/post')"/>
+          <q-btn 
+          v-if="$route.fullPath.includes('/post')"
+          class="q-px-none"
+          color="primary"          
+          label="Post"
+          size="sm"
+          style="width:20%"
+          unelevated
+          :ripple="false"/>
+      </q-toolbar>
+    </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
