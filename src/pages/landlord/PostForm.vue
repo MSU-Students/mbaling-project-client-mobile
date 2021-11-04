c98a65d0e444771c99226f364d5067a72c136869
Abdulwalie02 <abdulwalie5@gmail.com>
2021-11-04 13:45:05 +0800

    pull

--- src/pages/landlord/PostForm.vue
+++ src/pages/landlord/PostForm.vue
@@ -1,62 +1,52 @@
 <template>
-  <q-header style="height: 3rem; background-color: transparent">
-    <q-toolbar>
-      <q-btn
-        class="q-px-none"
-        color="black"
-        flat
-        icon="bi-arrow-left"
-        :ripple="false"
-        @click="$router.go(-1)"
-      />
-      <q-space />
-      <q-btn
-        class="q-mr-sm"
-        size="sm"
-        unelevated
-        :ripple="false"
-        color="primary"
-        label="post"
-      />
-    </q-toolbar>
-  </q-header>
+  <q-page>
+    <div
+      class=" q-mt-md landscape"
+      style="max-width: 100%; max-height: 100%"
+    >
+     <q-icon
+     v-if="!images[0]"
+      class="post-icon"
+      size="5rem"
+      color="grey"
+      name="bi-image"
+    />
 
-  <q-page class="defaultfont text-black">
-    <!-- POST PHOTO SECTION -->
-    <div class="bg-secondary" style="height: 20rem">
-      <div
-        v-if="!images[0]"
-        class="bg-transparent row items-center justify-evenly"
-        style="height: 20rem"
-      >
-        <q-icon size="xl" color="grey" name="bi-image" />
-      </div>
-      <q-carousel
-        v-else
-        arrows
-        class="bg-secondary"
-        animated
-        v-model="slide"
-        navigation
-        infinite
+    
+    <q-carousel
+    v-else
+      arrows
+      class="bg-secondary"
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      style="max-width: 100%; max-height: 100%"
+    >
+
+      <q-carousel-slide 
+      v-for="(image, index) in images" 
+      :key="image"
+      :name="index" 
+      :img-src="image"
+      />
+    </q-carousel>
+
+
+
+
+      <!-- <q-img
+        :src="imageData"
+        class=""
         style="max-width: 100%; max-height: 100%"
-      >
-        <q-carousel-slide
-          v-for="(image, index) in images"
-          :key="image"
-          :name="index"
-          :img-src="image"
-        />
-      </q-carousel>
+      /> -->
     </div>
 
-    <!-- UPLOAD AND CAMERA BUTTONS -->
-    <div class="q-mt-sm q-mb-lg q-px-lg">
-      <q-btn-group flat spread>
+    <div class="q-mt-xs q-px-lg">
+      <q-btn-group outline spread>
         <q-btn
+          class="text-subtitle2"
           flat
-          size="sm"
-          class="defaultfont-bold"
           color="primary"
           label="Upload"
           icon="bi-image"
@@ -70,77 +60,70 @@
           type="file"
           @change="onFileChange"
         />
-        <q-btn
-          flat
-          size="sm"
-          class="defaultfont-bold"
-          color="primary"
-          label="Capture"
-          icon="bi-camera"
-        />
+        <q-btn flat color="primary" label="Capture" icon="bi-camera" />
       </q-btn-group>
     </div>
 
-    <!-- INPUT FIELDS -->
-    <div class="q-mx-md">
-      <q-input
-        class="q-my-sm"
-        filled
-        dense
-        autogrow
-        v-model="title"
-        placeholder="Title"
-      />
+    <div>
+      <q-input 
+      class="q-ma-md " 
+      filled
+      dense
+      autogrow     
+      v-model="title" 
+      label="Title"/>
 
-      <div class="row q-my-sm">
+      <div class="row">
+      <q-input 
+      filled
+      dense
+      class="q-mx-md"
+      prefix="₱" 
+      v-model="fee" 
+      style="width:50%"
+      label="Fee" />
+
+      <q-checkbox dense v-model="negotiableBox" label="Negotiable" color="primary" />
+
+      </div>
+      <div class="column q-pl-lg q-ma-sm">
+       <q-checkbox 
+       class="q-mb-sm" 
+       dense 
+       v-model="pkBox" 
+       label="Private Kitchen" 
+       color="primary" />
+
+        <q-checkbox 
+        dense 
+        v-model="pcBox" 
+        label="Private CR" 
+        color="primary" />
+      </div>
+      
         <q-input
-          class="q-mr-md"
+         class="q-pa-md"
+          v-model="description"
           filled
-          dense
-          v-model="fee"
-          style="width: 50%"
-          placeholder="Fee (MONTHLY)"
+          type="textarea"
+          label="Description"
         />
-        <q-checkbox
-          dense
-          v-model="negotiableBox"
-          label="Negotiable"
-          color="primary"
-        />
-      </div>
-
-      <div class="column q-pl-lg q-mt-md">
-        <q-checkbox
-          class="q-mb-sm"
-          dense
-          v-model="pkBox"
-          label="Private Kitchen"
-          color="primary"
-        />
-        <q-checkbox dense v-model="pcBox" label="Private CR" color="primary" />
-      </div>
-
-      <q-input
-        class="q-mt-md"
-        v-model="description"
-        filled
-        type="textarea"
-        placeholder="Description"
-      />
     </div>
   </q-page>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue'
 export default {
-  setup() {
+  name: "",
+
+ setup () {
     return {
       negotiableBox: ref(false),
       pkBox: ref(false),
       pcBox: ref(false),
-      slide: ref(1),
-    };
+      slide: ref(1)
+    }
   },
   data() {
     return {
@@ -161,28 +144,37 @@ export default {
       var vm = this;
       for (var index = 0; index < files.length; index++) {
         var reader = new FileReader();
-        reader.onload = function (event) {
+        reader.onload = function(event) {
           const imageUrl = event.target.result;
-          vm.images.push(imageUrl);
-        };
+           vm.images.push(imageUrl);
+        }
         reader.readAsDataURL(files[index]);
       }
     },
     removeImage(index) {
-      this.images.splice(index, 1);
+      this.images.splice(index, 1)
     },
-    choosepicture() {
+     choosepicture() {
       this.$refs.fileInput.click();
     },
-  },
+  }
 };
 </script>
 
 <style>
+.landscape {
+  height: 15rem;
+  width: 23.4rem;
+}
 img {
   max-width: 100%;
   max-height: 100%;
   overflow: hidden;
   object-fit: contain;
+}
+.post-icon{
+  margin-left: 40%;
+  margin-top: 20%;
+
 }
 </style>
