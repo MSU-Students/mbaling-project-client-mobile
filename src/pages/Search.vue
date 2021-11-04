75436ddc2098ee8eb4b42a6024af4b8f58c33a4b
Abdul Moiz Solaiman <solaiman.am85@s.msumain.edu.ph>
2021-11-04 12:58:07 +0800

    QA initializing search page

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,7 +1,33 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-header class="bg-white text-black q-pa-md">
+    <q-input
+      :loading="loadingState"
+      square
+      filled
+      dense
+      v-model="search"
+      type="search"
+      placeholder="Search"
+    >
+      <template v-slot:prepend>
+        <q-btn flat round size="0.7rem">
+          <q-icon size="1.5rem" name="search" />
+        </q-btn>
+      </template>
+    </q-input>
+  </q-header>
 
-    <h1>SEARCH PAGE</h1>
-
+  <q-page class="text-black row items-center justify-evenly">
+    <h1>SEARCH</h1>
   </q-page>
 </template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { ref } from "vue";
+
+export default class Search extends Vue {
+  var search = ref("");
+  var loadingState = ref(false);
+}
+</script>
