683c8724d020555730454d52ce5dba4b32da34db
Abdulwalie02 <abdulwalie5@gmail.com>
2021-11-09 20:45:20 +0800

    update

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,5 +1,6 @@
 <template>
   <q-header class="bg-white text-black q-pa-md">
+    <q-form @submit="searchPosts()">
     <q-input
       v-model="search"
       square
@@ -12,38 +13,150 @@
     >
       <template v-slot:prepend>
         <q-btn flat round size="0.7rem" @click="searchPosts()">
-          <q-icon size="1.5rem" name="search" />
+          <q-icon size="1.5rem" type="submit" name="search" />
         </q-btn>
       </template>
     </q-input>
+    </q-form>
   </q-header>
 
-  <q-page class="q-px-md">
-    <q-tabs v-if="searchResult.length > 0" v-model="tabs" class="text-grey-9">
+  <q-page>
+    <q-tabs v-if="searchResult.length > 0" v-model="tabs" class="text-grey-9 full-width">
       <q-tab name="posts" label="Posts"> </q-tab>
       <q-tab name="users" label="Users" />
     </q-tabs>
 
+
+    <!-- POSTS TAB -->
     <q-tab-panels v-if="searchResult.length > 0" v-model="tabs" animated>
       <q-tab-panel class="q-pa-none q-pt-md" name="posts">
-        <div class="row items-center justify-center">
+        <div >
           <div
             v-for="(result, index) in searchResult"
             :key="index"
-            class="col-6 row items-center justify-center"
           >
-            <img :src="result.prfphoto" width="170" height="170" />
+            <q-item class="q-pt-md">
+          <q-item-section avatar top>
+            <q-avatar size="xl">
+              <img :src="result.prfphoto" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section top>
+            <q-item-label
+              lines="1"
+              class="defaultfont-semibold"
+              style="font-size: medium"
+            >
+              {{ result.fullname }}
+            </q-item-label>
+            <q-item-label lines="1" style="font-size: small">
+              {{ result.housingName }}
+            </q-item-label>
+            <span class="text-grey" style="font-size: xx-small">
+              {{ result.date }}
+            </span>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-btn
+              dense
+              flat
+              :ripple="false"
+              class="text-black"
+              icon="bi-three-dots"
+            />
+          </q-item-section>
+        </q-item>
+
+        <div class="q-my-xs">
+          <q-img :src="result.photo" />
+        </div>
+
+        <q-item class="">
+          <q-item-section class="defaultfont-medium">
+            <q-item-label lines="1" style="font-size: small">
+              {{ result.title }}
+            </q-item-label>
+            <q-item-label style="font-size: small">
+              {{ result.fee }} PHP monthly
+            </q-item-label>
+            <span
+              class="text-bold cursor-pointer"
+              style="font-size: x-small; text-align: right"
+            >
+              +View more
+            </span>
+          </q-item-section>
+        </q-item>
+        <q-separator inset color="primary" />
+         <q-item>
+          <div class="row">
+            <q-btn
+              flat
+              round
+              :ripple="false"
+              color="primary"
+              size="sm"
+              icon="bi-heart"
+            />
+            <q-btn
+              flat
+              round
+              :ripple="false"
+              color="primary"
+              size="sm"
+              icon="bi-chat-right"
+            />
+            <q-btn
+              flat
+              round
+              :ripple="false"
+              color="primary"
+              size="sm"
+              icon="bi-bookmark"
+            />
+          </div>
+          <q-space />
+          <span
+            class="defaultfont-medium absolute-bottom-right q-pr-md q-pb-md"
+            style="font-size: x-small"
+          >
+            {{ result.likes }} Likes • {{ result.bookmarks }} Bookmarks
+          </span>
+        </q-item>
+        <q-separator size="0.5rem" color="secondary" />
           </div>
         </div>
       </q-tab-panel>
+
+      <!-- USER TAB -->
       <q-tab-panel class="q-pa-none q-pt-md" name="users">
-        <div class="row items-center justify-center">
+        <div>
           <q-card
             v-for="(result, index) in searchResult"
             :key="index"
-            class="col-6 row items-center justify-center"
           >
-            <img :src="result.prfphoto" />
+             <q-item class="q-pt-md">
+          <q-item-section avatar top>
+            <q-avatar size="xl">
+              <img :src="result.prfphoto" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section top>
+            <q-item-label
+              lines="1"
+              class="defaultfont-semibold"
+              style="font-size: medium"
+            >
+              {{ result.fullname }}
+            </q-item-label>
+            <q-item-label lines="1" style="font-size: small">
+              {{ result.housingName }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
           </q-card>
         </div>
       </q-tab-panel>
@@ -55,7 +168,6 @@
 
 <script lang="ts">
 import { Vue, Options } from "vue-class-component";
-import { FiltersJS, Operation } from "filtersjs";
 
 // this is just a interface you need to practice this because TS required it a lot
 interface IPost {
@@ -98,7 +210,7 @@ export default class Search extends Vue {
       housingName: "Pirate King Apartment",
       prfphoto: "https://cdn.quasar.dev/img/avatar4.jpg",
       title:
-        "Family/Couple size Apartment room w/ Private kitchen and bathroom",
+        "Free boarding room size Apartment room w/ Private kitchen and bathroom",
       fee: "6,500",
       likes: 32,
       bookmarks: 4,
