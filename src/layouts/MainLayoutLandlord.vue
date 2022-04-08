96eae37eb93d10a6a5a05caf1d39d575b3a0f1a5
Abdul Moiz Solaiman <solaiman.am85@s.msumain.edu.ph>
2022-04-08 14:36:01 +0800

    main layout fix

--- src/layouts/MainLayoutLandlord.vue
+++ src/layouts/MainLayoutLandlord.vue
@@ -21,71 +21,59 @@
     <q-footer class="bg-primary text-white">
       <q-btn-group flat spread style="height: 3rem">
         <q-btn
-          v-if="$route.fullPath.includes('/landlord/home')"
           :ripple="false"
           size="lg"
           @click="$router.push('/landlord/home')"
         >
-          <i class="mbi-home-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/landlord/home')"
-        >
-          <i class="mbi-home"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/landlord/home')
+                ? 'mbi-home-fill'
+                : 'mbi-home'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/landlord/posts')"
           :ripple="false"
           size="lg"
-          @click="$router.push('/landlord/posts')"
+          @click="$router.push('/landlord/post')"
         >
-          <i class="mbi-post-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/landlord/posts')"
-        >
-          <i class="mbi-post"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/landlord/post')
+                ? 'mbi-post-fill'
+                : 'mbi-post'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/landlord/inbox')"
           :ripple="false"
           size="lg"
           @click="$router.push('/landlord/inbox')"
         >
-          <i class="mbi-inbox-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/landlord/inbox')"
-        >
-          <i class="mbi-inbox"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/landlord/inbox')
+                ? 'mbi-inbox-fill'
+                : 'mbi-inbox'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/landlord/account')"
           :ripple="false"
           size="lg"
           @click="$router.push('/landlord/account')"
         >
-          <i class="mbi-account-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/landlord/account')"
-        >
-          <i class="mbi-account"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/landlord/account')
+                ? 'mbi-account-fill'
+                : 'mbi-account'
+            "
+          ></i>
         </q-btn>
       </q-btn-group>
     </q-footer>
--- src/layouts/MainLayoutStudent.vue
+++ src/layouts/MainLayoutStudent.vue
@@ -18,64 +18,60 @@
       <router-view />
     </q-page-container>
 
-    <q-footer v-if="!$route.fullPath.includes('/post')" class="bg-primary text-white">
+    <q-footer
+      class="bg-primary text-white"
+    >
       <q-btn-group flat spread style="height: 3rem">
-        <q-btn
-          v-if="$route.fullPath.includes('/student/home')"
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/student/home')"
-        >
-          <i class="mbi-home-fill"></i>
-        </q-btn>
-        <q-btn v-else :ripple="false" size="lg" @click="$router.push('/student/home')">
-          <i class="mbi-home"></i>
+        <q-btn :ripple="false" size="lg" @click="$router.push('/student/home')">
+          <i
+            :class="
+              $route.fullPath.includes('/student/home')
+                ? 'mbi-home-fill'
+                : 'mbi-home'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/student/search')"
           :ripple="false"
           size="lg"
           @click="$router.push('/student/search')"
         >
-          <i class="mbi-search-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/student/search')"
-        >
-          <i class="mbi-search"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/student/search')
+                ? 'mbi-search-fill'
+                : 'mbi-search'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/student/inbox')"
           :ripple="false"
           size="lg"
           @click="$router.push('/student/inbox')"
         >
-          <i class="mbi-inbox-fill"></i>
-        </q-btn>
-        <q-btn v-else :ripple="false" size="lg" @click="$router.push('/student/inbox')">
-          <i class="mbi-inbox"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/student/inbox')
+                ? 'mbi-inbox-fill'
+                : 'mbi-inbox'
+            "
+          ></i>
         </q-btn>
 
         <q-btn
-          v-if="$route.fullPath.includes('/student/account')"
           :ripple="false"
           size="lg"
           @click="$router.push('/student/account')"
         >
-          <i class="mbi-account-fill"></i>
-        </q-btn>
-        <q-btn
-          v-else
-          :ripple="false"
-          size="lg"
-          @click="$router.push('/student/account')"
-        >
-          <i class="mbi-account"></i>
+          <i
+            :class="
+              $route.fullPath.includes('/student/account')
+                ? 'mbi-account-fill'
+                : 'mbi-account'
+            "
+          ></i>
         </q-btn>
       </q-btn-group>
     </q-footer>
