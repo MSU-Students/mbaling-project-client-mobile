97f5c697fd106ca3f4505e5baca391f303722fac
Abdulwalie Bashier <abdulwalie5@gmail.com>
2022-05-24 14:30:21 +0800

    return main postform layout

--- src/pages/general-post/PostForm.vue
+++ src/pages/general-post/PostForm.vue
@@ -5,11 +5,11 @@
         class="bg-grey-4 row items-center justify-evenly"
         style="height: 100%; border-radius: 2rem 2rem 0 0"
       >
-        <q-icon name="bi-image" size="8rem" color="grey" />
+        <q-icon name="bi-image" size="xl" color="grey" />
       </div>
     </div>
 
-    <!-- <div align="right" class="q-px-md q-py-sm row items-center">
+    <div align="right" class="q-px-md q-py-sm row items-center">
       <div class="col">
         <q-btn
           v-on:click="showClearBtn = !showClearBtn"
@@ -21,7 +21,6 @@
           class="q-ml-sm text-center"
           style="height: 2rem"
         />
-
         <Transition name="appear">
           <q-btn
             v-show="showClearBtn"
@@ -34,104 +33,51 @@
           />
         </Transition>
       </div>
-    </div> -->
-
-<!-- Upload Picture -->
-    <div class="row">
-    <div class="col">
-       <q-file
-        class="q-pr-lg text-primary"
-        outlined
-        label="Upload"
-        accept=".jpg, image/*"
-        v-model="imageAttachement"
-        item-aligned
-        style="width: 13rem;"
-      >
-        <template v-slot:prepend>
-          <q-icon name="album" />
-        </template>
-      </q-file>
     </div>
-    <div class="col">
-      <q-file
-        class="q-pr-lg text-primary"
-        outlined
-        disable
-        label="Capture"
-        accept=".jpg, image/*"
-        item-aligned
-        style="width: 13rem;"
-        @click="alert()"
-      >
-        <template v-slot:prepend>
-          <q-icon name="camera" />
-        </template>
-      </q-file>
-    </div>
-    </div>
-<!--  -->
 
     <div class="q-mt-sm q-px-md">
       <q-input
-        v-model="inputPost.title"
-        filled
+        v-model="title"
+        autogrow
         dense
-        type="text"
         placeholder="Title"
-        input-class="text-on-left"
+        input-class="text-center"
         style="font-size: medium"
       />
-      <div class="row">
-        <div class="col">
       <q-input
-        class="q-mt-sm"
-        v-model="inputPost.fee"
-        prefix="P"
+        v-model="fee"
         dense
-        filled
-        type="number"
         placeholder="Monthly Fee"
-        input-class="text-on-left"
+        input-class="text-center"
+        class="q-mt-xs q-px-xl"
         style="font-size: medium"
       />
-      </div>
-      <div class="col q-pt-sm q-pl-sm">
-          <q-checkbox
-            label="Negotiable"
-            v-model="inputPost.Negotiable"
-            color="primary"
-            style="font-size: medium"
-          />
-      </div>
-      </div>
 
-        <div class="q-pt-md q-pl-lg">
+      <div class="q-px-md q-mt-md row items-center">
+        <div align="center" class="col">
           <q-checkbox
             label="Private Kitchen"
-            v-model="inputPost.prvKitchen"
-            color="primary"
-            style="font-size: medium"
+            v-model="pkBox"
             dense
+            color="primary"
           />
         </div>
-        <div class="q-pt-md q-pl-lg">
+        <div align="center" class="col">
           <q-checkbox
             label="Private CR"
-            v-model="inputPost.prvCR"
-            color="primary"
-            style="font-size: medium"
+            v-model="pcBox"
             dense
+            color="primary"
           />
         </div>
+      </div>
 
       <q-input
-        v-model="inputPost.description"
+        v-model="description"
         type="textarea"
-        filled
         placeholder="Description"
         class="q-mt-md q-pb-lg"
-        style="font-size: md;"
+        style="font-size: small"
       />
     </div>
 
@@ -160,7 +106,6 @@
         no-caps
         color="primary"
         style="height: 3rem; width: 5rem"
-        @click="createPost()"
       />
     </div>
   </q-footer>
